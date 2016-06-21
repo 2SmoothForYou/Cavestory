62b684c90f52dc640fe4de8f028298baaee5d086
Jaime Ricart <jaimericart@jaimes-macbook-pro.local>
2016-06-21 12:55:47 -0500

    Initial Commit

---
+++ Cavestory/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cavestory
+//
+//  Created by Jaime Ricart on 6/21/16.
+//  Copyright © 2016 Jaime Ricart. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
